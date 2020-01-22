cab325dd84c160b7159047561c1ad3ba46611449
lloydnabs <32464813+lloydnabs@users.noreply.github.com>
2020-01-22 19:50:00 +0000

    Update modpackSelector.h

--- src/modpackSelector.h
+++ src/modpackSelector.h
@@ -12,11 +12,11 @@ void HandleMultiModPacks(u64 titleid/*,bool showMenu = true*/);
 void console_print_pos(int x, int y, const char *format, ...);
 void copy_dir(const char *src, char *dest)
 {
-    if (!create_dir(dest))
+    if (!createdir(dest))
         return;
         
     struct dirent *de;
-    DIR *dir = open_dir(src);
+    DIR *dir = opendir(src);
     if (!dir)
         return;
 
@@ -27,13 +27,13 @@ void copy_dir(const char *src, char *dest)
         snprintf(buffer, sizeof(buffer), "%s/%s", dest, de->d_name);
 
         // check if the file is a directory.
-        if (is_dir(de->d_name))
+        if (dir(de->d_name))
         {
-            create_dir(buffer);
+            createdir(buffer);
             copy_dir(de->d_name, de->d_name);
         }
         else
-            copy_file(de->d_name, buffer);
+            void copy_file(de->d_name, buffer);
     }
     closedir(dir);
 }
