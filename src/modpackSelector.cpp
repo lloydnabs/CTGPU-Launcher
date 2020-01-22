a055cd2808ca10b3a89cb1cbab943506ab98d1ed
lloydnabs <32464813+lloydnabs@users.noreply.github.com>
2020-01-22 19:16:20 +0000

    Update modpackSelector.cpp

--- src/modpackSelector.cpp
+++ src/modpackSelector.cpp
@@ -24,59 +24,6 @@
 
 #define TEXT_SEL(x, text1, text2)           ((x) ? (text1) : (text2))
 
-void copy_dir(const char *src, char *dest)
-{
-    if (!create_dir(dest))
-        return;
-        
-    struct dirent *de;
-    DIR *dir = open_dir(src);
-    if (!dir)
-        return;
-
-    char buffer[0x200] = {0};
-
-    while ((de = readdir(dir)))
-    {
-        snprintf(buffer, sizeof(buffer), "%s/%s", dest, de->d_name);
-
-        // check if the file is a directory.
-        if (is_dir(de->d_name))
-        {
-            create_dir(buffer);
-            copy_dir(de->d_name, de->d_name);
-        }
-        else
-            copy_file(de->d_name, buffer);
-    }
-    closedir(dir);
-}
-
-void copy_file(const char *src, char *dest)
-{
-    FILE *srcfile = fopen(src, "rb");
-    if (!srcfile)
-    {
-        return;
-    }
-
-    FILE *newfile = fopen(dest, "wb");
-    if (!newfile)
-    {
-        fclose(srcfile);
-        return;
-    }
-    
-    void *buf = malloc(0x800000);
-    size_t bytes; // size of the file to write (8MiB or filesize max)
-
-    while (0 < (bytes = fread(buf, 1, 0x800000, srcfile)))
-        fwrite(buf, bytes, 1, newfile);
-    free(buf);
-    
-    fclose(srcfile);
-    fclose(newfile);
-}
 
 void HandleMultiModPacks(u64 titleID/*,bool showMenu*/) {
 	gModFolder[0] = 0;
