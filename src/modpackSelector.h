ea5abe94167d8a1fcc250c02f794704b0d145fe6
lloydnabs <32464813+lloydnabs@users.noreply.github.com>
2020-01-22 19:36:20 +0000

    Update modpackSelector.h

--- src/modpackSelector.h
+++ src/modpackSelector.h
@@ -5,7 +5,9 @@
 extern "C" {
 #endif
 #include <dynamic_libs/os_types.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 void HandleMultiModPacks(u64 titleid/*,bool showMenu = true*/);
 void console_print_pos(int x, int y, const char *format, ...);
 void copy_dir(const char *src, char *dest)
